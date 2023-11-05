984c3c8f8c489a2290ba7026b0ba08828f4aace5
tkdrms96 <a18rama@naver.com>
2023-11-05 23:48:57 +0900

    프로젝트 세팅

---
+++ src/main/java/com/sign/signdirector/SignDirectorApplication.java
@@ -0,0 +1,13 @@
+package com.sign.signdirector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignDirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignDirectorApplication.class, args);
+	}
+
+}
